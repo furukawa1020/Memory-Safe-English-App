b01cc5857c0a4dad15e6c1cec394e90796fc210c
furukawa1020 <f.kotaro.0530@gmail.com>
2026-04-29 10:06:56 +0900

    Add review queue route to problem bank

--- services/proxy-rs/src/routes.rs
+++ services/proxy-rs/src/routes.rs
@@ -23,6 +23,7 @@ pub fn build_router(state: AppState) -> Router {
         .route("/problem-bank/activity", get(problems::problem_activity))
         .route("/problem-bank/insights", get(problems::problem_insights))
         .route("/problem-bank/recommend", get(problems::recommend_problems))
+        .route("/problem-bank/review-queue", get(problems::review_queue))
         .route("/problem-bank/stats", get(problems::problem_bank_stats))
         .route(
             "/problem-bank/:id",
